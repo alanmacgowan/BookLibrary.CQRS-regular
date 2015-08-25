9c6123936236cc7035b96abbabb816b39eb798a6
alanmacgowan <alanmacgowan@gmail.com>
2015-08-25 20:38:18 -0300

    Added model classes

---
+++ BookLibrary.CommandStack/Model/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BookLibrary.CommandStack.Model
+{
+    public class Book
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ISBN { get; set; }
+
+        public int Year { get; set; }
+
+        public virtual Author Author { get; set; }
+
+    }
+}
