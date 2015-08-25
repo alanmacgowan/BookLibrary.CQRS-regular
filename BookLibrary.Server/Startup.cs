24dacb6b62aae60601486ee1a9e98d3e5bfea4ff
alanmacgowan <alanmacgowan@gmail.com>
2015-08-25 17:45:47 -0300

    Initial Commit

---
+++ BookLibrary.Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookLibrary.Server.Startup))]
+namespace BookLibrary.Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
